ddc695a40bb3329d69b1cf6b539ca2940aee6ae5
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-06-08 21:13:37 +0530

    Removed Bugs

--- SMT.py
+++ SMT.py
@@ -1,9 +1,11 @@
-class student:
+try:
+ class student:
     def __init__(self,name,roll,gender,dept):
         self.name = name
         self.roll = roll
         self.gender = gender
         self.dept = dept
+        
     def display(self):
         print("STUDENT DETAILS")
         print("==========================================")
@@ -12,6 +14,7 @@ class student:
     def add(self,Nname,Nroll,Ngen,Ndept):
         stud = student(Nname,Nroll,Ngen,Ndept)
         ls.append(stud)
+        
     def search(self,searcroll):
      found = False
      for i in ls:
@@ -23,39 +26,58 @@ class student:
             break
      if not found:
         print("Student Details Not Found")
-    def delete(self,delroll):
-        deldet = stud.search(delroll)
-        del deldet
-ls = []
-name = input("Enter The name of the student: ")
-roll = int(input("Enter Students Roll.no: "))
-Gender = input("Enter Students Gender: ")
-Dept = input("Enter department: ")
-stud = student(name,roll,Gender,Dept)
-ls.append(stud)
-while True:
+        
+    def delete(self, delroll):
+     found = False
+     for i in ls:
+        if i.roll == delroll:
+            ls.remove(i)
+            found = True
+            print("Student Deleted Successfully")
+            break
+     if not found:
+        print("Student Not Found")
+            
+ ls = []
+ name = input("Enter The name of the student: ")
+ roll = int(input("Enter Students Roll.no: "))
+ Gender = input("Enter Students Gender: ")
+ Dept = input("Enter department: ")
+ stud = student(name,roll,Gender,Dept)
+ ls.append(stud)
+
+ while True:
     choice = int(input("Enter Operation number(1-display,2-add,3-search,4-delete):"))
+    
     match choice:
         case 1:
             for i in ls:
                 i.display()
+        
         case 2:
             Nname = input("Enter Student name to be added: ")
             Nroll = int(input("Enter roll no: "))
             Ngen = input("Enter gender: ")
             Ndept = input("Enter Department: ")
             stud.add(Nname,Nroll,Ngen,Ndept)
+        
         case 3:
             searcroll = int(input("Enter students rollNo: "))
             stud.search(searcroll)
+        
         case 4:
             delroll = int(input("Enter student roll no to be deleted:"))
             stud.delete(delroll)
+        
         case _:
             print("Invalid Choice")
     ch = input("Do you wish to continue (y/n): ")
+    
     if ch.lower() != 'y':
         break
+except Exception as e:
+    print("Error",e)
+
  
     
         
